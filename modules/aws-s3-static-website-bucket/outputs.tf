ef9915d780da17cd4b6ce6e48598969b9985a9aa
cloudpractice5760 <cloudpractice5760@gmail.com>
2024-10-09 14:20:53 +0530

    Update outputs.tf

--- modules/aws-s3-static-website-bucket/outputs.tf
+++ modules/aws-s3-static-website-bucket/outputs.tf
@@ -1,1 +1,13 @@
+# Output variable definitions
+
+output "arn" {
+  description = "ARN of the bucket"
+  value       = aws_s3_bucket.s3_bucket.arn
+}
+
+output "name" {
+  description = "Id of the bucket"
+  value       = aws_s3_bucket.s3_bucket.id
+}
+
 
